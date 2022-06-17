3d878a2457fed9d22044fef850eaa19305149788
kezia-yifru <yifrukezia@gmail.com>
2022-06-16 21:49:33 -0700

    commit

--- Kitten_Website/JS/kitten.js
+++ Kitten_Website/JS/kitten.js
@@ -26,4 +26,22 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+} 
+
+// This code will create close the contact form when the user clicks off of it
+// The first step is to add an event listener for any clicks on the website
+document.addEventListener("click", function(event){
+    // Here we state that if the click happens on the cancel button OR anywhere that is not the contact form AND the click does not happen on any element with the contact class then call the closeForm() function
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+        closeForm()
+    }
+}, false ) 
+
+// These functions open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
 }
